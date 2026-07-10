5ec472c9d6289d5d5748f0c255d6cfb762160038
holly-soldier <kiseki01@gmail.com>
2026-07-11 00:50:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-const CACHE_NAME = 'atelier-v3';
-const ASSETS = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
-self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())));
-self.addEventListener('activate', (e) => e.waitUntil(caches.keys().then((ks) => Promise.all(ks.map((k) => { if (k !== CACHE_NAME) return caches.delete(k); }))).then(() => self.clients.claim())));
-self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request))));
+const CACHE = 'atelier-v4';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['index.html','manifest.json']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
